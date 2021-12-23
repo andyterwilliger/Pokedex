9610e8a98c67a14b58d7ab93910d0e524b965f12
Andy Terwilliger <andy@terwilliger1@gmail.com>
2021-12-23 11:09:08 -0600

    index route working

--- server.js
+++ server.js
@@ -8,6 +8,31 @@ const app = express();
 
 const port = 3000;
 
+//middleware
+
+
+
+
+
+
+
+
+
+
+//routes
+
+app.get('/pokemon', (req, res) => {
+    res.send("I have never seen Pokemon or owned any merch")
+});
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
     console.log('listening...')
 });
