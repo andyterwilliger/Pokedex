0474eb0af6e9fd055381645f4578e3984c6b3265
Andy Terwilliger <andy@terwilliger1@gmail.com>
2021-12-27 17:21:37 -0600

    added update route

--- server.js
+++ server.js
@@ -31,26 +31,38 @@ app.use(methodOverride('_method'));
 
 //routes INDUCES
 
+app.get('/', (req, res) => res.redirect('/pokemon'));
+
+//index
 app.get('/pokemon/', (req, res) => {
     res.render('index.ejs', {
         allPokemon: pokemon,
     })
 });
 
+//new
 app.get('/pokemon/new', (req, res) => {
     res.render('new.ejs')
 });
 
-app.post('/pokemon', (req, res) => {
-    console.log(req.body)
-    pokemon.unshift(req.body)
-    res.redirect('/pokemon')
-});
-
-
+//delete
 app.delete('/pokemon/:indexOfPokemonArray', (req, res) => {
     pokemon.splice(req.params.indexOfPokemonArray, 1);
     res.redirect('/pokemon');
+});
+
+//update
+app.put('/pokemon/:id', (req, res) => {
+    pokemon[req.params.indexOfPokemonArray] = req.body;
+    res.redirect('/pokemon');
+});
+
+
+
+//create
+app.post('/pokemon', (req, res) => {
+    pokemon.unshift(req.body)
+    res.redirect('/pokemon')
 });
 
 
@@ -58,6 +70,7 @@ app.delete('/pokemon/:indexOfPokemonArray', (req, res) => {
 //  res.send ('new data about pokes')
 //});
 
+//edit
 app.get('/pokemon/:indexOfPokemonArray/edit', (req, res) => {
     res.render('edit.ejs', {
         pokeItem: pokemon[req.params.indexOfPokemonArray],
@@ -65,7 +78,7 @@ app.get('/pokemon/:indexOfPokemonArray/edit', (req, res) => {
     });
 });
 
-
+//show
 app.get('/pokemon/:indexOfPokemonArray', (req, res) => {
     res.render('show.ejs', { pokeItem: pokemon[req.params.indexOfPokemonArray] });
 });
